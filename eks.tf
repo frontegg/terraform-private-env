ad25e986ccd6317a5eedc24d6302305d540cc07f
Eyal Tam <eyal.tam@frontegg.com>
2025-06-25 18:57:20 +0300

    update eks node tag - fix for nginx

--- eks.tf
+++ eks.tf
@@ -106,6 +106,7 @@ module "eks" {
   node_security_group_tags = {
     "karpenter.sh/discovery" = local.kubernetes_cluster_name
     "service"                = "frontegg private environment"
+    "kubernetes.io/cluster/${local.kubernetes_cluster_name}" = "shared"
   }
 }
 
